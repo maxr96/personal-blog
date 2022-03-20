bf80e232202371b12c0da47f535b2c8dcc1b9eec
Max <maxr96@gmail.com>
2022-03-20 13:06:02 +0100

    :tada: Add about page and use Link component for navigation.

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import Container from "../components/container"
+import Layout from "../components/layout"
+
+const About = () => {
+    return (
+        <Layout>
+            <Container>
+                <div>About</div>
+            </Container>
+        </Layout>
+    )
+} 
+
+export default About
