b9ef79bf2a8e3a1421f3ec5d057f87d6fd638149
Max <maxr96@gmail.com>
2022-04-03 16:03:30 +0200

    :wastebasket: Remove not used meta information.

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { HOME_OG_IMAGE_URL } from '../lib/constants'
 
 const Meta = () => {
   return (
@@ -22,22 +21,23 @@ const Meta = () => {
         href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
+      {/* TODO: Replace Safari pinned tab icon and OG image. */}
+      {/* <link
         rel="mask-icon"
         href="/favicon/safari-pinned-tab.svg"
         color="#000000"
-      />
+      /> */}
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      {/* <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js.`}
+        content={`Max Dev personal full stack developer blog.`}
       />
       {/* OG Image is used for thumbnail pictures, e.g. Facebook. Change to a proper one later*/}
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
     </Head>
   )
 }
