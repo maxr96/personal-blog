47307f1912d0cd798338973b36f8989cbe5620d2
Maksym Radchuk <maxr96@gmail.com>
2022-08-13 18:55:21 -0400

    :recycle: Extract item component from navbar

---
+++ components/navbar-item.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+type Props = {
+  href: string
+  currentPathName: string
+  title: string
+}
+const NavbarItem = ({ href, currentPathName, title }: Props) => {
+  return (
+    <Link href={href}>
+      <a
+        className={` ${
+          currentPathName === href ? ' md:text-blue-700' : ''
+        } block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
+        aria-current="page"
+      >
+        {title}
+      </a>
+    </Link>
+  )
+}
+
+export default NavbarItem
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,15 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import NavbarItem from './navbar-item'
 
 const Navbar = () => {
+  const router = useRouter()
+  const pages = [
+    { href: '/', title: 'Posts' },
+    { href: '/about-me', title: 'About Me' },
+    { href: '/my-projects', title: 'My Projects' },
+    { href: '/contact-me', title: 'Contact Me' }
+  ]
   return (
     <nav className="sticky top-0 bg-white border-gray-200 px-2 sm:px-4 py-5 dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -51,37 +60,14 @@ const Navbar = () => {
         </button>
         <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            <li>
-              <Link href="/">
-                <a
-                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                  aria-current="page"
-                >
-                  Posts
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="about-me">
-                <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  About Me
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="my-projects">
-                <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  My Projects
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="contact-me">
-                <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  Contact Me
-                </a>
-              </Link>
-            </li>
+            {pages.map(({ href, title }) => (
+              <NavbarItem
+                key={href}
+                href={href}
+                currentPathName={router.pathname}
+                title={title}
+              />
+            ))}
             {/* TODO: Dark mode switch button here */}
           </ul>
         </div>
