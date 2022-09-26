da9c73cd1c3394c2823b645a7919338f8af0964e
Maksym Radchuk <maxr96@gmail.com>
2022-09-27 00:16:07 +0200

    📝 Correct multiple typos in the blog post

--- lib/blogImages.ts
+++ lib/blogImages.ts
@@ -2,8 +2,8 @@ import { StaticImageData } from 'next/future/image';
 import dynamicRouting from '../public/assets/blog/dynamic-routing/cover.jpg'
 import helloWorld from '../public/assets/blog/hello-world/cover.jpg'
 import preview from '../public/assets/blog/preview/cover.jpg'
-import personalProjectTipps from '../public/assets/blog/personal-project-tipps/cover.jpg'
+import personalProjectTips from '../public/assets/blog/personal-project-tipps/cover.jpg'
 
-const blogImages: {[key: string]: StaticImageData} = {dynamicRouting, helloWorld, preview, personalProjectTipps}
+const blogImages: {[key: string]: StaticImageData} = {dynamicRouting, helloWorld, preview, personalProjectTips}
 
 export default blogImages;
