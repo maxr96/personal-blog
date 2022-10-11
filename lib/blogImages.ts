b233e81dc2727d667d20df9a0bec49e344eb2ae5
Maksym Radchuk <maxr96@gmail.com>
2022-10-11 20:45:06 +0200

    ⚡️ Reduce image size and clean up unneded images.

--- lib/blogImages.ts
+++ lib/blogImages.ts
@@ -1,9 +1,6 @@
 import { StaticImageData } from 'next/future/image';
-import dynamicRouting from '../public/assets/blog/dynamic-routing/cover.jpg'
-import helloWorld from '../public/assets/blog/hello-world/cover.jpg'
-import preview from '../public/assets/blog/preview/cover.jpg'
 import personalProjectTips from '../public/assets/blog/personal-project-tipps/cover.jpg'
 
-const blogImages: {[key: string]: StaticImageData} = {dynamicRouting, helloWorld, preview, personalProjectTips}
+const blogImages: {[key: string]: StaticImageData} = {personalProjectTips}
 
 export default blogImages;
